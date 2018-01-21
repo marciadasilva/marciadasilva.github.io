73f87b3481ce8917c558ad189cd3111033c68552
Marcia <Marcia>
2018-01-21 15:42:18 -0200

    services-detail ok

--- vessencia-frontend/assets/js/script.js
+++ vessencia-frontend/assets/js/script.js
@@ -1,16 +1,5 @@
 window.onload = function() {
 
-  // const widthSize = document.getElementById("body").offsetWidth;
-  // const row = Math.floor(widthSize/310);
-  //
-  // for (i = 0; i < row*3; i++) {
-  //   const element = document.createElement("div");
-  //   const color = '#'+(Math.random()*0xFFFFFF<<0).toString(16);
-  //   element.classList.add("item");
-  //   element.style.backgroundColor = color;
-  //   insertElement(element, color);
-  // }
-
   const items = document.querySelectorAll('.item');
 
   items.forEach(item => (changeBackgroundColor(item)));
@@ -21,19 +10,3 @@ function changeBackgroundColor(item){
   const color = '#'+(Math.random()*0xFFFFFF<<0).toString(16);
   item.style.backgroundColor = color;
 }
-
-// function insertElement(element, color){
-//   const position = Math.floor(Math.random() * 100);
-//   const image = document.createElement("img");
-//   const iTag = document.createElement("i");
-//
-//   image.src = `https://picsum.photos/310/200/?image=${position}`;
-//   iTag.classList.add("fa");
-//   iTag.classList.add("fa-camera");
-//   iTag.style.backgroundColor = color;
-//
-//   element.appendChild(image);
-//   element.appendChild(iTag);
-//
-//   document.getElementById('container').appendChild(element);
-// }
