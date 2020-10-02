1bf34e1abc4a90b70a0750eac9f1ab4c2b5f4fd8
Igor Haugg <igor-haugg@hotmail.com>
2020-10-02 13:16:23 -0400

    updating function to arrow function and cleaning up repo

--- image-gallery/asset/js/app.js
+++ image-gallery/asset/js/app.js
@@ -1,62 +1,49 @@
-window.onload = start();
-
-function start() {
+const start = () => {
   detail();
   gallery();
-}
-
-function detail() {
-  const url = 'https://ron-swanson-quotes.herokuapp.com/v2/quotes';
-  const details = Array.from(document.querySelectorAll('.item__details'));
-  details.forEach((detail)=>(setText(detail, url)));
-}
-
-function setText(detail, url) {
-  const element = document.createElement("span");
-
-  fetch(url)
-   .then(function(req){
-     req.json().then(function(data){
-       element.textContent = data[0];
-       detail.appendChild(element);
-     })
-   })
-   .catch(function(){
-     alert("ERROR!")
-   });
-
-}
-
-function gallery() {
-    const sizes = [
-      // ' ',
-      'item--medium',
-      'item--large'
-      // 'item--full'
-    ];
-
-    const items = Array.from(document.querySelectorAll('.item'));
-
-    items.forEach((item)=>(setSize(item, sizes)));
-
 };
 
-function setSize(item, sizes) {
-  let random = Math.floor(Math.random() * sizes.length + 1);
+const detail = () => {
+  const url = 'https://ron-swanson-quotes.herokuapp.com/v2/quotes';
+  const details = Array.from(document.querySelectorAll('.item__details'));
+  details.forEach((detail) => setText(detail, url));
+};
 
-  // if (random == 2) {
-  //   random = Math.floor(Math.random() * sizes.length + 1);
-  // }
+const setText = async (detail, url) => {
+  const element = document.createElement('span');
+  try {
+    const req = await fetch(url);
+    const data = await req.json();
+    element.textContent = data[0];
+    detail.appendChild(element);
+  } catch (error) {
+    alert('ERROR!');
+  }
+};
 
-  if (random !== sizes.length + 1){
+const gallery = () => {
+  const sizes = ['item--medium', 'item--large'];
+  const items = Array.from(document.querySelectorAll('.item'));
+  items.map((item) => setSize(item, sizes));
+};
+
+const setSize = (item, sizes) => {
+  const random = Math.floor(Math.random() * sizes.length + 1);
+
+  if (random !== sizes.length + 1) {
     item.classList.add(sizes[random]);
   }
 
-  item.addEventListener('click', function (){
+  item.addEventListener('click', () => {
     item.classList.toggle('item--full');
   });
 
   const position = Math.floor(Math.random() * 500);
 
-  item.setAttribute('style', `background-image: url(https://picsum.photos/1024/768/?image=${position})`);
+  item.setAttribute(
+    'style',
+    `background-image: url(https://picsum.photos/1024/768/?image=${position})`
+  );
 };
+
+window.onload = start();
